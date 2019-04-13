44a1e8869581436b90b5dc1f1c15a81ce0528763
alanjbates <38515640+alanjbates@users.noreply.github.com>
2019-04-13 18:58:24 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Google_Cloud_Function_FTP_getfiles
+
+This is a Google Cloud Function that was written to retrieve all .zip files from a 3rd party FTP site. 
+
+The function is triggered by an http request every 5 minutes from Google Cloud Scheduler.
+
+It is deployed using Google Cloud Build from git repo merge triggers.
+
+This is a simple and effective serverless way to pull files from FTP sites into GCP storage buckets on a cron schedule.
+
+*NOTE* You will want to consider if clear text authentication credentials in your source code.  For production deployment, encrypted or obscured authentication credentials are recommended. 
---
+++ main.py
@@ -0,0 +1,37 @@
+from ftplib import FTP
+from fs_gcsfs import GCSFS
+
+def getfiles(request):
+
+    #set gcs params
+    gcs_bucket_name = 'your-gcs-bucket-name'
+	gcsfs = GCSFS(bucket_name=gcs_bucket_name)
+    gcs_bucket_string = 'gs://' + gcs_bucket_name + '/'
+	
+    #set ftp params
+    ftp = FTP('ftp.your-ftp-site.com')
+    ftp.login('your-username', 'your-password')
+    
+    # get files within the root directory into list
+    filenames = ftp.nlst()
+	
+	#loopp through each each file and copy into gcs if it is a .zip file (your specific use case may be different)
+	#here you can look for specific file names or remove the if statement if you are pulling everything form the ftp source
+    for filename in filenames:
+        if '.zip' in filename:
+            gcs_target_filename = gcs_bucket_string + filename
+            print('Retrieving file: ' + filename)
+			#this line pulls the file from the FTP site and writes it to a file in the GCS bucket
+            ftp.retrbinary('RETR '+ filename, gcsfs.open(filename, 'wb').write)
+            print('Successfully created: ' + gcs_target_filename)
+            ftp.delete(filename)
+            print('Deleted from remote host: ' + filename)
+        else:
+			#write line for stackdriver logging
+            print('No .Zip files found on remote host.')
+    
+    #close ftp connection
+    ftp.quit()
+    
+	#write line for stackdriver logging
+    print('done')
