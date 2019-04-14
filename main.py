741e6098a69af8493b10ff940d3b0633c395f869
Alan Bates <38515640+alanjbates@users.noreply.github.com>
2019-04-13 20:35:00 -0400

    formatting fix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def getfiles(request):
 
     #set gcs params
     gcs_bucket_name = 'your-gcs-bucket-name'
-	gcsfs = GCSFS(bucket_name=gcs_bucket_name)
+    gcsfs = GCSFS(bucket_name=gcs_bucket_name)
     gcs_bucket_string = 'gs://' + gcs_bucket_name + '/'
 	
     #set ftp params
@@ -15,23 +15,23 @@ def getfiles(request):
     # get files within the root directory into list
     filenames = ftp.nlst()
 	
-	#loopp through each each file and copy into gcs if it is a .zip file (your specific use case may be different)
-	#here you can look for specific file names or remove the if statement if you are pulling everything form the ftp source
+    #loop through each each file and copy into gcs if it is a .zip file (your specific use case may be different)
+    #here you can look for specific file names or remove the if statement if you are pulling everything form the ftp source
     for filename in filenames:
         if '.zip' in filename:
             gcs_target_filename = gcs_bucket_string + filename
             print('Retrieving file: ' + filename)
-			#this line pulls the file from the FTP site and writes it to a file in the GCS bucket
+	    #this line pulls the file from the FTP site and writes it to a file in the GCS bucket
             ftp.retrbinary('RETR '+ filename, gcsfs.open(filename, 'wb').write)
             print('Successfully created: ' + gcs_target_filename)
             ftp.delete(filename)
             print('Deleted from remote host: ' + filename)
         else:
-			#write line for stackdriver logging
+	    #write line for stackdriver logging
             print('No .Zip files found on remote host.')
     
     #close ftp connection
     ftp.quit()
     
-	#write line for stackdriver logging
+    #write line for stackdriver logging
     print('done')
